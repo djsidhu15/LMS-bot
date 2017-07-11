eb18f0f0e7288c5f1b5c3e7ae76f233364928ad7
Sidharth J <djsidhu15@gmail.com>
2017-07-12 02:32:45 +0530

    First commit

--- src/message.php
+++ src/message.php
@@ -35,7 +35,90 @@ function replyMessage ($message) {
   * Or: Update your DB
   * etc...
   */
+  $server = "localhost";
+$dbusername = "root";
+$dbpassword = "root";
+$dbname = "phplms";
+if ($response->action->slug == 'greetings') {
+  // Do your code
+echo "Greetings User!";
 
+
+$usernametext = "sid";
+$passwordtext = "sid";
+array_push($response->replies, "Welcome to LMS. You can enquire about your marks as of now.");
+
+$conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+else{
+$sql = "SELECT * from register where username = 'sid' and password = 'sid';";
+$result = $conn->query($sql);
+
+//if ($result->num_rows > 0) {
+if($result!=null){
+  array_push($response->replies, "Inside rows > 0");
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      array_push($response->replies, "Inside While");
+        //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        if($usernametext==$row["username"]){
+            if($passwordtext==$row["password"]){
+                //session_start();
+                echo "User found! Username = ".$row['username']."\n";
+    echo "Password = ".$row['password']."\n";
+    array_push($response->replies, "Welcome to LMS. You can enquire about your marks as of now.");
+                //$_SESSION['username']=$usernametext;
+                //$_SESSION['name']=$row["name"];
+                //header('Location: Auth.php');
+                $conn->close();
+                //exit;
+            }
+        }
+    }
+}
+else{
+    echo "0 results";
+    $conn->close();
+}
+}
+}
+
+if ($response->action->slug == 'online-test-marks') {
+  $sql = "";
+  $test_number = $response->memory->number->scalar;
+  $username = $response->memory->username->value;
+  if($username==null){
+    array_push($response->replies, "Log in 1st to get the marks");
+  }
+  //array_push($response->replies, "Test number = ".$test_number);
+  else if($test_number==null){
+    //array_push($response->replies, "Test number null");
+
+    
+  }else{
+    $sql = "select * from onlinetest".$test_number." where username = '".$username."';";
+    $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+else{
+$result = $conn->query($sql);
+
+//if ($result->num_rows > 0) {
+if($result!=null){
+  array_push($response->replies, "Your onlinetest".$test_number." marks :");
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+      array_push($response->replies, $row["cname"]." - ".$row["marks"]);
+  }
+  }
+}
+}
+}
   /*
   * Add each replies received from API to replies stack
   */
